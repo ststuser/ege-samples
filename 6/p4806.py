bb98570ac85b2cb76faf465e774764895e16c22e
ss <anony@mail.com>
2022-02-12 19:55:50 +0300

    fix

--- 6/p4806.py
+++ 6/p4806.py
@@ -1,13 +1,11 @@
+import math
 
 cnt = 0
 
-for xs in range(1, 354261000 - 987 + 1):
-    x = xs
-    n = (354261000 - 987 - xs) // 3 + 1
-    n_l = (xs + 987) + 3 * (n - 1)
-    n_n = 987 + 8*n
-    if n_n//1000 == 231:
-        print(n_n)
+for x in range(1, 354261000 - 987 + 1):
+    k = math.ceil((354261000 - 987 - x) / 3) + 1
+    n = 987 + 8*(k-1)
+    if n//1000 == 231:
         cnt += 1
 
 print(cnt)
