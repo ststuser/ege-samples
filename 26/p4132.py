769d85efbf4f25ace5cc8f4d01ef9ddf4edd6f6f
ss <anony@mail.com>
2022-04-11 17:00:27 +0300

    add 26 example

---
+++ 26/p4132.py
@@ -0,0 +1,49 @@
+file_content = open('p4132.txt', 'r').read()
+file_lines = file_content.split('\n')
+disk_volume, disk_number, _ = file_lines[0].split()
+
+archives = [int(x) for x in file_lines[1:] if x]
+archives.sort(reverse=True)
+print(archives[:5])
+
+disks = [int(disk_volume) for _ in range(int(disk_number))]
+disk_to_write = 0
+not_packed = []
+
+
+def change_disk():
+    global disk_to_write
+    disk_to_write += 1
+    if disk_to_write >= len(disks):
+        disk_to_write = 0
+
+# for archive in archives:
+#     start_disk = disk_to_write
+#     if disks[disk_to_write] >= archive:
+#         disks[disk_to_write] -= archive
+#         change_disk()
+#     else:
+#         change_disk()
+#         while disks[disk_to_write] < archive and disk_to_write != start_disk:
+#             change_disk()
+#         if disks[disk_to_write] >= archive:
+#             disks[disk_to_write] -= archive
+#             change_disk()
+#         else:
+#             not_packed.append(archive)
+
+
+for archive in archives:
+    start_disk = disk_to_write
+    while disks[disk_to_write] < archive:
+        change_disk()
+        if disk_to_write == start_disk:
+            break
+
+    if disks[disk_to_write] >= archive:
+        disks[disk_to_write] -= archive
+        change_disk()
+    else:
+        not_packed.append(archive)
+
+print(sum(not_packed), len(not_packed))
